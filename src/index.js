5161eb8271933334c9653efcaa93b221e958a3f2
Boris Espinosa <borisem.dev@gmail.com>
2025-09-12 20:32:41 -0300

    cron.js added

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import cors from "cors"
 import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
 import { connectDB } from "./lib/db.js"
+import job from "./lib/cron.js"
 
 const app = express()
 const PORT = process.env.PORT
 
+job.start() // Start the cron job
 app.use(express.json())
 app.use(cors())
 app.use("/api/auth", authRoutes)
