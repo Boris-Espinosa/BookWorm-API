cea0668905926839be60d8e185f9020d411db287
Boris Espinosa <borisem.dev@gmail.com>
2025-09-14 20:01:51 -0300

    bookroutes add

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
+import bookRoutes from "./routes/bookRoutes.js"
 import { connectDB } from "./lib/db.js"
 import job from "./lib/cron.js"
 
@@ -12,6 +13,7 @@ job.start() // Start the cron job
 app.use(express.json())
 app.use(cors())
 app.use("/api/auth", authRoutes)
+app.use('/api/books', bookRoutes)
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT)
