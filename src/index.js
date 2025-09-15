78a31f637ca6a02d1ad77adaf9436f4afe6da740
Boris Espinosa <borisem.dev@gmail.com>
2025-09-14 21:17:36 -0300

    limit change

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const app = express()
 const PORT = process.env.PORT
 
 job.start() // Start the cron job
-app.use(express.json())
+app.use(express.json({ limit: '50mb' })) // Aumentar límite para imágenes
+app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cors())
 app.use("/api/auth", authRoutes)
 app.use('/api/books', bookRoutes)
