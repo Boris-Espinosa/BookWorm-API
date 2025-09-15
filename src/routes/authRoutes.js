943af9c3fc9af2e67bb97a4946c04bdcb3278a39
Boris Espinosa <borisem.dev@gmail.com>
2025-09-15 20:45:13 -0300

    created at fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -95,7 +95,8 @@ router.post("/login", async(req, res) => {
                 _id: user._id,
                 username: user.username,
                 email: user.email,
-                profileImage: user.profileImage
+                profileImage: user.profileImage,
+                createdAt: user.createdAt,
             }
         })
 
